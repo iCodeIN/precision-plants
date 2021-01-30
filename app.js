e1a722174e48d3383d1d680d717424f1352293fd
Tay <tayk.dev@gmail.com>
2021-01-29 19:10:53 -0800

    Add files via upload
    Fixed the slideshow. It works now! I was only supposed to add one image element && update the SRC with JavaScript. I was calling to the images in HTML and JS. Along with other mistakes like using "source" instead of "src" and wrong variable spelling.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // variables
 var i = 0; // index counter
-var image = [];
+var images = [];
 var time = 3000;
 
 // Image List
-image[0] = 'spath.jpg';
-image[1] = 'dracena.jpg';
-image[2] = 'marble_queen.jpg';
+images[0] = 'Spath.jpg';
+images[1] = 'dracena.jpg';
+images[2] = 'pothos.jpg';
 
 // Change Image
 function changeImg(){
-    document.slide.source = images[i];
+    document.slide.src = images[i];
     
-    if (i < image.length - 1) {
+    if (i < images.length - 1) {
         i++;
     } else {
         i = 0;
     }
 
-    setTimeout ("changeImg", time);
+    setTimeout ('changeImg()', time);
 }
 
 window.onLoad = changeImg();
